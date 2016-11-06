8e633678c3f0cac1bba0f7e2127c1e063f006fd4
Nikola Stjelja <nikola@epikia.eu>
2016-11-06 13:47:32 +0100

    Initial multiple deployments solution

---
+++ scripts/deployment.ps1
@@ -0,0 +1,37 @@
+function createResourceGroupIsNotExists($resourceGroup, $location){
+  $result = Get-AzureRmResourceGroup | Select -ExpandProperty ResourceGroupName | Where-Object {$_ -eq $resourceGroup}
+  
+  if ($result){
+    Write-Host "Resource group $($resourceGroup) found."
+    return;
+  }
+   
+  Write-Host "Creating resource group $($resourceGroup) in location $($location)"
+  New-AzureRmResourceGroup -Name $resourceGroup -Location $location 
+}
+
+#Create the resource group  for the sql server
+createResourceGroupIsNotExists "svibor" "West Europe"
+
+$sqlServerResult = New-AzureRmResourceGroupDeployment -ResourceGroupName "svibor" -TemplateFile "../templates/sqlserver.json" -Name "sqlserverdeployment"
+$sqlServerResult | Out-String
+
+if ($sqlServerResult.ProvisioningState -eq "Failed"){
+    Write-Host "Something went wrong , stopping deployment"
+    exit 1
+}
+
+#Create the resource group for the App Service/Plan
+createResourceGroupIsNotExists "perun" "West Europe"
+
+$parameters = @{"SqlServerFQDN" = $sqlServerResult.Outputs.Item("sqlServerFQDN").Value;
+                "SqlServerName" = $sqlServerResult.Outputs.Item("SqlServerName").Value; 
+                "SqlServerAdminLogin" = $sqlServerResult.Outputs.Item("SqlServerAdminLogin").Value;
+                "SqlServerPassword" = $sqlServerResult.Outputs.Item("SqlServerPassword").Value;
+                "DatabaseName" = $sqlServerResult.Outputs.Item("DatabaseName").Value;}
+
+$parameters | Out-String
+exit
+
+$appServiceResult = New-AzureRmResourceGroupDeployment -ResourceGroupName "perun" -TemplateFile "../templates/appservice.json" -Name "sqlserverdeployment" -TemplateParameterObject $parameters
+$appServiceResult | Out-String
